743a5f4271a55fd646080a17b4514b59a18a91f3
David Janke <david@numbermumbler.com>
2014-10-23 13:52:51 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Conversion Tracking module
+PrestaShop module that inserts customer conversion tracking code from Facebook and AdWords
+
+## Compatibility
+This module should work with PrestaShop versions 1.5 and 1.6
+
+## Installation
+Put this folder in the _modules_ directory of your PrestaShop installation. Then, install and activate it in Modules > Modules in PrestaShop's admin site.
---
+++ conversiontracking.php
@@ -0,0 +1,56 @@
+<?php
+/*
+* 2007-2014 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2014 PrestaShop SA
+*  @license	http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+class conversiontracking extends Module {
+	public function __construct() {
+		$this->name = 'conversiontracking';
+		$this->tab = 'checkout';
+		$this->version = '1.0.0';
+		$this->ps_versions_compliancy = array('min' => '1.5');
+		$this->author = 'David Janke';
+
+		parent::__construct();
+
+		$this->displayName = $this->l('Conversion Tracking');
+		$this->description = $this->l('Track customer convertions from ad services');
+	}
+
+	public function install() {
+		if (Shop::isFeatureActive()) {
+			Shop::setContext(Shop::CONTEXT_ALL);
+		}
+		return parent::install();
+	}
+
+	public function uninstall() {
+		return parent::uninstall();
+	}
+
+	// Admin screen
+	public function getContent() {
+	}
+
+	// Hooks
+}
