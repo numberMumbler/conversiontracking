efcaf964019fb1a33a09c9b2353220002b9a0897
David Janke <david@numberMumbler.com>
2015-02-26 14:48:35 -0700

    restart using PS module generator

--- conversiontracking.php
+++ conversiontracking.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
 * 2007-2014 PrestaShop
 *
 * NOTICE OF LICENSE
@@ -11,64 +11,212 @@
 * If you did not receive a copy of the license and are unable to
 * obtain it through the world-wide-web, please send an email
 * to license@prestashop.com so we can send you a copy immediately.
+*
 * DISCLAIMER
 *
 * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
 * versions in the future. If you wish to customize PrestaShop for your
 * needs please refer to http://www.prestashop.com for more information.
 *
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2014 PrestaShop SA
-*  @license	http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2014 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-class conversiontracking extends Module {
-	public function __construct() {
+if (!defined('_PS_VERSION_'))
+	exit;
+
+class conversiontracking extends Module
+{
+	protected $config_form = false;
+
+	public function __construct()
+	{
 		$this->name = 'conversiontracking';
-		$this->tab = 'checkout';
-		$this->version = '1.0.0';
-		$this->ps_versions_compliancy = array('min' => '1.5');
+		$this->tab = 'advertising_marketing';
+		$this->version = '0.0.1';
 		$this->author = 'David Janke';
+		$this->need_instance = 0;
+
+		/**
+		 * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+		 */
+		$this->bootstrap = true;
 
 		parent::__construct();
 
 		$this->displayName = $this->l('Conversion Tracking');
-		$this->description = $this->l('Track customer convertions from ad services');
+		$this->description = $this->l('Include and manage tracking scripts for various marketing services');
+
+		$this->confirmUninstall = $this->l('Conversion tracking scripts will be removed. Are you sure you want to uninstall?');
 	}
 
-	public function install() {
-		if (Shop::isFeatureActive()) {
-			Shop::setContext(Shop::CONTEXT_ALL);
-		}
+	/**
+	 * Don't forget to create update methods if needed:
+	 * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+	 */
+	public function install()
+	{
+		Configuration::updateValue('conversiontracking_LIVE_MODE', false);
+
+		include(dirname(__FILE__).'/sql/install.php');
+
 		return parent::install() &&
+			$this->registerHook('header') &&
+			$this->registerHook('backOfficeHeader') &&
 			$this->registerHook('displayOrderConfirmation');
 	}
 
-	public function uninstall() {
+	public function uninstall()
+	{
+		Configuration::deleteByName('conversiontracking_LIVE_MODE');
+
+		include(dirname(__FILE__).'/sql/uninstall.php');
+
 		return parent::uninstall();
 	}
 
-	// Admin screen
-	public function getContent() {
-		// TODO: implement me
-		return '<center>Under Construction</center>';
+	/**
+	 * Load the configuration form
+	 */
+	public function getContent()
+	{
+		/**
+		 * If values have been submitted in the form, process.
+		 */
+		$this->_postProcess();
+
+		$this->context->smarty->assign('module_dir', $this->_path);
+
+		$output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
+
+		return $output.$this->renderForm();
 	}
 
-	// Hooks
-	public function hookDisplayOrderConfirmation($params) {
-		$fbTrackers = array(array('id' => '6018368151287'),
-							   array('id' => '6015469448538'));
-		$adwordsTrackers = array(array('id' => '988298912', 'label' => 'QOXQCODEhVcQoP2g1wM'));
+	/**
+	 * Create the form that will be displayed in the configuration of your module.
+	 */
+	protected function renderForm()
+	{
+		$helper = new HelperForm();
 
-		$orderTotal = $params['total_to_pay'];
+		$helper->show_toolbar = false;
+		$helper->table = $this->table;
+		$helper->module = $this;
+		$helper->default_form_language = $this->context->language->id;
+		$helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
 
-		$this->smarty->assign(array(
-			'fbTrackers' => $fbTrackers,
-			'adwordsTrackers' => $adwordsTrackers,
-			'orderTotal' => $orderTotal
-		));
+		$helper->identifier = $this->identifier;
+		$helper->submit_action = 'submitconversiontrackingModule';
+		$helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+			.'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+		$helper->token = Tools::getAdminTokenLite('AdminModules');
 
-		return $this->display(__FILE__, 'displayOrderConfirmation.tpl');
+		$helper->tpl_vars = array(
+			'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
+			'languages' => $this->context->controller->getLanguages(),
+			'id_language' => $this->context->language->id,
+		);
+
+		return $helper->generateForm(array($this->getConfigForm()));
+	}
+
+	/**
+	 * Create the structure of your form.
+	 */
+	protected function getConfigForm()
+	{
+		return array(
+			'form' => array(
+				'legend' => array(
+				'title' => $this->l('Settings'),
+				'icon' => 'icon-cogs',
+				),
+				'input' => array(
+					array(
+						'type' => 'switch',
+						'label' => $this->l('Live mode'),
+						'name' => 'conversiontracking_LIVE_MODE',
+						'is_bool' => true,
+						'desc' => $this->l('Use this module in live mode'),
+						'values' => array(
+							array(
+								'id' => 'active_on',
+								'value' => true,
+								'label' => $this->l('Enabled')
+							),
+							array(
+								'id' => 'active_off',
+								'value' => false,
+								'label' => $this->l('Disabled')
+							)
+						),
+					),
+					array(
+						'col' => 3,
+						'type' => 'text',
+						'prefix' => '<i class="icon icon-envelope"></i>',
+						'desc' => $this->l('Enter a valid email address'),
+						'name' => 'conversiontracking_ACCOUNT_EMAIL',
+						'label' => $this->l('Email'),
+					),
+					array(
+						'type' => 'password',
+						'name' => 'conversiontracking_ACCOUNT_PASSWORD',
+						'label' => $this->l('Password'),
+					),
+				),
+				'submit' => array(
+					'title' => $this->l('Save'),
+				),
+			),
+		);
+	}
+
+	/**
+	 * Set values for the inputs.
+	 */
+	protected function getConfigFormValues()
+	{
+		return array(
+			'conversiontracking_LIVE_MODE' => Configuration::get('conversiontracking_LIVE_MODE', true),
+			'conversiontracking_ACCOUNT_EMAIL' => Configuration::get('conversiontracking_ACCOUNT_EMAIL', 'contact@prestashop.com'),
+			'conversiontracking_ACCOUNT_PASSWORD' => Configuration::get('conversiontracking_ACCOUNT_PASSWORD', null),
+		);
+	}
+
+	/**
+	 * Save form data.
+	 */
+	protected function _postProcess()
+	{
+		$form_values = $this->getConfigFormValues();
+
+		foreach (array_keys($form_values) as $key)
+			Configuration::updateValue($key, Tools::getValue($key));
+	}
+
+	/**
+	* Add the CSS & JavaScript files you want to be loaded in the BO.
+	*/
+	public function hookBackOfficeHeader()
+	{
+		$this->context->controller->addJS($this->_path.'js/back.js');
+		$this->context->controller->addCSS($this->_path.'css/back.css');
+	}
+
+	/**
+	 * Add the CSS & JavaScript files you want to be added on the FO.
+	 */
+	public function hookHeader()
+	{
+		$this->context->controller->addJS($this->_path.'/js/front.js');
+		$this->context->controller->addCSS($this->_path.'/css/front.css');
+	}
+
+	public function hookDisplayOrderConfirmation()
+	{
+		/* Place your code here. */
 	}
 }
